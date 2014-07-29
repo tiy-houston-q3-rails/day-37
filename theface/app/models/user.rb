e111c271c416c483eca39cb95778b19840f918ce
Jesse Wolgamott <jesse@comalproductions.com>
2014-07-29 12:08:22 -0500

    initial devise user

---
+++ theface/app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :omniauthable, :omniauth_providers => [:facebook]
+  
+  end
